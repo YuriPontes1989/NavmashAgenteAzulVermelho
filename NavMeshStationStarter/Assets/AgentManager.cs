1a41ad9d6900869e5a6abd47741f7467fdfc1f12
BloodyValentine <69511939+YuriPontes1989@users.noreply.github.com>
2022-05-13 23:12:20 -0300

    Update AgentManager.cs

--- NavMeshStationStarter/Assets/AgentManager.cs
+++ NavMeshStationStarter/Assets/AgentManager.cs
@@ -9,15 +9,19 @@ public class AgentManager : MonoBehaviour
     void Start()
     {
         agents = GameObject.FindGameObjectsWithTag("ai");
+        // a variavel agente vai  achar os gameobject com a tag definada como "ia"
     }
     // Update is called once per frame
     void Update()
     {
+        // se eu apertar o botão esquerdo do mouse
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
+            // se o raio, com origem na posição do mouse, cruza com um colisor na distância máxima de 100
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
+            // para cada objeto em A coloque o destino do componente Aicontrol como o ponto mouse
                 foreach (GameObject a in agents)
                     a.GetComponent<AIControl>().agent.SetDestination(hit.point);
             }
